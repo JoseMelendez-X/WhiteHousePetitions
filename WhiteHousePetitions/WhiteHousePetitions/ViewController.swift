7dee566dd8ec212a17b068c319e69b0e0021b6f2
Jose Melendez <JoseM_Melendez@icloud.com>
2017-08-24 00:13:48 -0400

    Organized files, ready to begin

---
+++ WhiteHousePetitions/WhiteHousePetitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteHousePetitions
+//
+//  Created by Jose Melendez on 8/23/17.
+//  Copyright © 2017 JoseMelendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
